e0481bc1aebb35cf2c3f9c67eea2a1f6497e3423
Andrew Nee <ndrewnee@gmail.com>
2021-05-29 04:58:27 +0500

    Add solutions for ch11

--- ch11/exercise_a.js
+++ ch11/exercise_a.js
@@ -1,4 +1,4 @@
 // Write a natural transformation that converts `Either b a` to `Maybe a`
 
 // eitherToMaybe :: Either b a -> Maybe a
-const eitherToMaybe = undefined;
+const eitherToMaybe = (e) => (e.isLeft ? nothing : Maybe.of(e.$value))
